923a15ec04724a5609ca70f7ba6d1e8e7c954168
Vrvevwvbe <107951403+Vrvevwvbe@users.noreply.github.com>
2022-06-23 04:34:12 +0300

    Add files via upload

--- NIXA/Player/start.py
+++ NIXA/Player/start.py
@@ -1,175 +1,101 @@
 import asyncio
+from time import time
+from datetime import datetime
+from config import BOT_USERNAME
+from config import GROUP_SUPPORT, UPDATES_CHANNEL, START_PIC
+from NIXA.filters import command
+from NIXA.command import commandpro
 from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors import MessageNotModified
-from Zaid.main import Test, bot as Client
-from config import START_PIC, UPDATES_CHANNEL, GROUP_SUPPORT
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from NIXA.main import bot as Client
 
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ( week , 60 * 60 * 24 * 7),
+    ( day , 60 * 60 * 24),
+    ( hour , 60 * 60),
+    ( min , 60),
+    ( sec , 1)
+)
 
-ALIVE_PIC = START_PIC
-HOME_TEXT = "🤼 **لك يعمري ههلو [{}](tg://user?id={})** \n\n❤️‍🔥 انا اقوى بوت ** متطور لتشغيل الاغاني**. \n**لتشغيل الاغاني في المحموعات يمكنك معرفة استخدامي عن طريق زر الاوامر**"
-HELP_TEXT = """
-🏷️ **لوحة التحكم** :
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return  inf 
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append( {} {}{} 
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return  ,  .join(parts)
+    
+   
 
-\u2022 يمكنك معرفة جميع الاوامر عن طريق الازرار الادناه.
-"""
-
-
-USER_TEXT = """
-🏷️ **هذا قائمة الاوامر** :
-
-\u2022 - تابع الاوامر في الاسفل ↓
-
-\u2022 -› .شغل - بالرد على ملف صوتي او اسم أغنية
-\u2022 -› .اصعد - لصعود حساب المساعد في المكالمة
-\u2022 -› .انزل - لنزول المساعد من المكالمة
-\u2022 -› .تخطي - لتخطي اغنية في التشغيل
-\u2022 -› .كافي - لايقاف تشغيل جميع الاغاني
-\u2022 -› .اضبط - لضبط صوت حساب المساعد
-\u2022 -› .فيديو - بالرد على مقطع فيديو او اسم فيديو
-\u2022 -› .الانتضار - لرؤية قائمة الانتضار التشغيل
-\u2022 -› .ابحثلي - لبحث عن فيديو من اليوتيوب
-\u2022 -› .بحث - لتحميل اغنية من اليوتيوب
-\u2022 -› .كتم - لكتم صوت المساعد 
-\u2022 -› .بنك - لإضهار بنك البوت
-\u2022 -› .انضم - لدعوة حساب المساعد
-
-. شكراً لقرائتك الاوامر - أتمنى لك يوماً تعيساً 🦴
-"""
-
-
-@Client.on_callback_query()
-async def cb_handler(client: Client, query: CallbackQuery):
-    if query.data=="help":
-        buttons = [
-            [                
-                InlineKeyboardButton("🤼 الاومر", callback_data="users"),          
-            ],
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_PIC}",
+        caption=f"""**اهلآ بك 
+في اقوا بوت متطور مميزاته مامطروقه.
+لتشغيل الاغاني في المكالمات الصوتيه..
+**""",
+    reply_markup=InlineKeyboardMarkup(
             [
-                InlineKeyboardButton("🔙 رجوع", callback_data="home"),               
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                HELP_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
-
-    elif query.data=="home":
-        get_me = await client.get_me()
-        USERNAME = get_me.username
-        buttons = [
-            [
-                InlineKeyboardButton("➕ اضفني الى مجموعتك", url='https://t.me/{USERNAME}?startgroup=true'),
-            ],
-            [
-                InlineKeyboardButton(" قناة المطور", url=f"https://t.me/{GROUP_SUPPORT}"),
-                InlineKeyboardButton("🏷️ المطور", url=f"https://t.me/{UPDATES_CHANNEL}"),
-            ],
-                          
-            [
-                InlineKeyboardButton("🤼 الاوامر", callback_data="help"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                HOME_TEXT.format(query.from_user.first_name, query.from_user.id),
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
-
-    elif query.data=="users":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 الاوامر", callback_data="help"),
-                InlineKeyboardButton("🤷 رجوع", callback_data="close"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                USER_TEXT.format(query.from_user.first_name, query.from_user.id),
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
-
-    elif query.data=="admins":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 الاوامر", callback_data="help"),
-                InlineKeyboardButton("🤷 مسح", callback_data="close"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(ADMIN, reply_markup=reply_markup)
-        except MessageNotModified:
-            pass
-
-    elif query.data=="raid":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 الاوامر", callback_data="help"),
-                InlineKeyboardButton("🤷 مسح", callback_data="close"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                RAID_TEXT.format(query.from_user.first_name, query.from_user.id),
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
-
-    elif query.data=="spam":
-        buttons = [
-            [
-                InlineKeyboardButton("🔙 الاوامر", callback_data="help"),
-                InlineKeyboardButton("🤷 مسح", callback_data="close"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        try:
-            await query.edit_message_text(
-                SPAM_TEXT.format(query.from_user.first_name, query.from_user.id),
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
-
-    elif query.data=="close":
-        try:
-            await query.message.delete()
-            await query.message.reply_to_message.delete()
-        except:
-            pass
-
-
-@Client.on_message(filters.command(["start"]) & filters.private)
+                [
+                    InlineKeyboardButton(
+                        "➕ ❰ اضفني الى مجموعتك ❱ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "الاوامر الاساسيه", url=f"https://telegra.ph/%D8%B3%D9%88%D8%B1%D8%B3-%D9%83%D9%88%D8%A8%D8%B1%D8%A7-%D8%A7%D9%84%D8%A7%D9%81%D8%B6%D9%84-06-23"
+                    ),
+                    InlineKeyboardButton(
+                        "", url="https://t.me/QABNADLIB"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "قناة السورس", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                    InlineKeyboardButton(
+                        "كروب المساعدة", url=f"https://t.me/{GROUP_SUPPORT}"
+                    )
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/stats"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
-    get_me = await client.get_me()
-    USERNAME = get_me.username
-    buttons = [
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/bf9f444677e4d565542a6.jpg",
+        caption=f"""اهلا بك انا اقوا بوت في التليكرام لتشغيل الاغاني.""",
+        reply_markup=InlineKeyboardMarkup(
             [
-                InlineKeyboardButton("➕ اضفني الى مجموعتك ➕", url=f'https://t.me/{USERNAME}?startgroup=true'),
-            ],
-            [               
-                InlineKeyboardButton("قناة المطور", url=f"https://t.me/{UPDATES_CHANNEL}"),
-                InlineKeyboardButton("🏷️ المطور", url=f"https://t.me/{UPDATES_CHANNEL}"),
-            ],
-            
-            [
-                InlineKeyboardButton("🤼 الاوامر", callback_data="help"),
+                [
+                    InlineKeyboardButton(
+                        "Source Cobra", url=f"https://t.me/VFF35")
+                ]
             ]
-            ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_photo(photo=f"{ALIVE_PIC}", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+        ),
+    )
 
+
+@Client.on_message(command(["repo", "source"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/187646e964cd12329f1de.jpg",
+        caption=f"""هلاو عمري يمكنك تنصيب بوت خاص بك تواصل مع المطور""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝐒𝐈𝐅 𝐂𝐎𝐁𝐑𝐀", url=f"https://t.me/QABNADLIB")
+                ]
+            ]
+        ),
+    )
